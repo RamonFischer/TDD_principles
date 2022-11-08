4336436f3b5e11d17778c99bc1a4f5db0babf7d2
ramon.fischer <ramonfischer1999@gmail.com>
2022-11-08 16:42:08 -0300

    Done testing and creating resource test

---
+++ src/test/java/com/example/TDD/resources/UserResourceTest.java
@@ -0,0 +1,33 @@
+package com.example.TDD.resources;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserResourceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void updateUsers() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
