1a2b011ee80e3ae9c16dfaba9d77176853d38a2b
ramon.fischer <ramonfischer1999@gmail.com>
2022-11-11 17:02:23 -0300

    finished TDD bug fix

--- src/main/java/com/example/TDD/entities/UserDTO.java
+++ src/main/java/com/example/TDD/entities/UserDTO.java
@@ -3,9 +3,6 @@ package com.example.TDD.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
