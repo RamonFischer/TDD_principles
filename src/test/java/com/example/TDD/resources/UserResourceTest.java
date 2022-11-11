84cce3dc150aca80bf80913d4a5996707cb69dca
ramon.fischer <ramonfischer1999@gmail.com>
2022-11-11 05:00:16 -0300

    finished some more TDD methods for Controller

--- src/test/java/com/example/TDD/resources/UserResourceTest.java
+++ src/test/java/com/example/TDD/resources/UserResourceTest.java
@@ -10,7 +10,12 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -31,6 +36,8 @@ class UserResourceTest {
     public static final String EMAIL = "ramon@hotmaillll";
     public static final String PASSWORD = "aa";
 
+    public static final int INDEX = 0;
+
     private User user;
 
     private UserDTO userDTO;
@@ -49,7 +56,6 @@ class UserResourceTest {
 
         assertNotNull(response);
         assertNotNull(response.getBody());
-
         assertEquals(ResponseEntity.class,response.getClass());
         assertEquals(UserDTO.class,response.getBody().getClass());
         assertEquals(ID,response.getBody().getId());
@@ -60,11 +66,33 @@ class UserResourceTest {
     }
 
     @Test
-    void saveUser() {
+    void whenFindAllThenReturnSuccess() {
+        when(serviceImp.findAll()).thenReturn(List.of(user));
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = controller.findAll();
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(ArrayList.class, response.getBody().getClass());
+        assertEquals(EMAIL,response.getBody().get(0).getEmail());
+        assertEquals(UserDTO.class,response.getBody().get(INDEX).getClass());
     }
 
     @Test
-    void findAll() {
+    void whenSaveUserThenReturnSuccess() {
+        when(serviceImp.saveUser(userDTO)).thenReturn(user);
+        when(mapper.map(any(),any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = controller.saveUser(userDTO);
+
+        assertNotNull(response);
+        assertNull(response.getBody());
+        assertEquals(ResponseEntity.class,response.getClass());
+        assertEquals(HttpStatus.CREATED,response.getStatusCode());
+        assertNotNull(response.getHeaders().get("Location"));
     }
 
     @Test
